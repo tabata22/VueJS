a1fe0e9113b4fabe9be8b559bba58babec5299e7
ntabatadze <n.tabatadze@ssa.gov.ge>
2018-12-13 12:11:58 +0400

    First Look

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <p>{{ post.name }}</p>
+      <p>{{ post.body }}</p>
+    </li>
+  </ul>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  created() {
+    axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
