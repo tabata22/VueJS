7727a6420d32d24070125f37d15b76523f543afc
ntabatadze <n.tabatadze@ssa.gov.ge>
2018-12-13 12:43:39 +0400

    update

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -1,11 +1,19 @@
 <template>
 
-  <ul>
-    <li v-for="post in posts" :key="post.id">
-      <p>{{ post.name }}</p>
-      <p>{{ post.body }}</p>
-    </li>
-  </ul>
+  <div>
+        <input type="text" v-model="input.firstname" placeholder="First Name" />
+        <input type="text" v-model="input.lastname" placeholder="Last Name" />
+        <button v-on:click="sendData()">Send</button>
+        <br />
+        <br />
+
+        <ul>
+          <li v-for="post in posts" :key="post.id">
+            <p>{{ post.name }}</p>
+            <p>{{ post.body }}</p>
+          </li>
+        </ul>
+  </div>
 
 </template>
 
@@ -16,6 +24,10 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      input: {
+        firstname: "",
+        lastname: ""
+      },
       posts: [],
       errors: []
     }
@@ -23,12 +35,20 @@ export default {
 
   created() {
     axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    .then(response => {
-      this.posts = response.data
+    .then(result => {
+      this.posts = result.data
     })
     .catch(e => {
       this.errors.push(e)
     })
+  },
+
+  methods: {
+    sendData() {
+      axios({ method: "POST", "url": "https://httpbin.org/post", "data": this.input, "headers": { "content-type": "application/json" } }).then(error => { 
+        console.error(error);
+      });
+    }
   }
 }
 </script>
